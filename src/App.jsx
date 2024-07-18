b369135deaa819f16650e72e8251008d8a6d55e6
Rebakure <kevinrebakure@gmail.com>
2024-07-18 12:56:10 +0200

    done ✅ - feat/form

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import NavBar from "./components/NavBar"
+import Form from "./components/Form"
 
 function App() {
   return(
     <div className="w-[550px] mx-auto shadow-lg">
       <NavBar />
+      <main className="px-[37px] py-[28px]">
+        <Form />
+      </main>
     </div>
   )
 }
---
+++ src/components/Form.jsx
@@ -0,0 +1,19 @@
+export default function Form() {
+    return (
+      <form className="grid grid-cols-2 gap-x-[30px] gap-y-[15px]">
+        <div className="space-y-[3px]">
+          <p className="font-medium">Top text</p>
+          <input className="border rounded-[5px] px-[10px] py-[7px]" type="text" name="" id="" />
+        </div>
+        <div className="space-y-[3px]">
+          <p className="font-medium">Bottom text</p>
+          <input className="border rounded-[5px] px-[10px] py-[7px]" type="text" name="" id="" />
+        </div>
+        <input
+          className="col-span-2 rounded-[5px] py-[10px] bg-gradient-to-tr from-[#672280] to-[#A626D3] text-white"
+          type="submit"
+          value="Get new meme image 🖼️"
+        />
+      </form>
+    );
+}
