f73f356a21e33def9a9c91f2987eb72443f104e1
Rebakure <kevinrebakure@gmail.com>
2024-07-19 14:36:01 +0200

    memes array

--- src/memesData.js
+++ src/memesData.js
@@ -1,11 +1,13 @@
 export let fetched = false
 
 export async function memesData() {
- if (!fetched) {
-   console.log("fetched");
+//  if (!fetched) {
+  //  console.log("fetched");
    const response = await fetch("https://api.imgflip.com/get_memes");
    const data = await response.json();
-   fetched = !fetched
-   return data.data.memes;
- }
+  //  fetched = !fetched
+   console.log(data.data.memes.length);
+//  }
 }
+
+memesData()
