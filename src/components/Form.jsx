1a130f466a3b7124b8fbe097c355ee4f56650f56
Rebakure <kevinrebakure@gmail.com>
2024-07-19 14:47:40 +0200

    Update Form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,27 +1,18 @@
 import { useState } from "react";
 import Meme from "./Meme";
-import { fetched, memesData } from "../memesData";
+import memes from "../memes";
 
 export default function Form() {
   const [image, setImage] = useState("./memeimg.png");
-  const [arr, setArr] = useState([{ url: "./memeimg.png" }]);
-  function submitted(e) {
-    e.preventDefault();
-    if (!fetched) {
-      memesData().then((data) => {
-        setImage(data[0].url);
-        setArr(data.sort(() => Math.random() - 0.5));
-      });
-    } else {
-      setArr(arr.sort(() => Math.random() - 0.5));
-      setImage(arr[0].url);
-    }
+  function clicked() {
+    const random = Math.floor(Math.random() * (memes.length - 0 + 1)) + 0;
+    setImage(memes[random].url);
   }
 
   return (
     <>
       <form
-        onSubmit={submitted}
+        onSubmit={(e) => e.preventDefault()}
         className="grid grid-cols-2 gap-x-[30px] gap-y-[15px] text-[14px]"
       >
         <div className="space-y-[3px]">
@@ -42,11 +33,12 @@ export default function Form() {
             id=""
           />
         </div>
-        <input
+        <button
+          onClick={clicked}
           className="col-span-2 rounded-[5px] bg-gradient-to-tr from-[#672280] to-[#A626D3] py-[10px] text-[16px] text-white"
-          type="submit"
-          value="Get new meme image 🖼️"
-        />
+        >
+          Get new meme image 🖼️
+        </button>
       </form>
 
       <Meme img={image} />
